1825254be9b6ff4646e832dca1f0cbd9f16a9909
PapankaSanjana <papankasanjana@gmail.com>
2026-05-06 22:21:54 +0530

    Update browser_fixture.py

--- fixtures/browser_fixture.py
+++ fixtures/browser_fixture.py
@@ -1,9 +1,18 @@
-import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome()   # ✅ Selenium auto-manages driver
-    driver.maximize_window()
+
+    options = Options()
+
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
+
     yield driver
+
     driver.quit()
